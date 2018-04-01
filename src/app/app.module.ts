e86c1cf6e92d32829319f866ed09fbc2ebb05f48
Garrett Smiley <garrettsmiley@msn.com>
2018-04-01 12:06:48 -0400

    created global navbar and resolved style errors

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { routing } from './app.routing';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent
   ],
   imports: [
-    BrowserModule
+    FormsModule,
+    ReactiveFormsModule,
+    BrowserModule,
+    routing,
+    HttpModule
   ],
   providers: [],
   bootstrap: [AppComponent]
