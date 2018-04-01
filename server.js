b8556e5c83ced5958fbf095e7467855b896069f3
Garrett Smiley <garrettsmiley@msn.com>
2018-04-01 11:24:00 -0400

    fixed errors, scaffolded navbar component, imported global styles, and created server

---
+++ server.js
@@ -0,0 +1,59 @@
+// Get dependencies
+const express = require('express');
+const path = require('path');
+const http = require('http');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+// Get our API routes
+const api = require('./server/routes/api');
+const guides = require('./server/routes/guides');
+const user = require('./server/routes/user');
+const create = require('./server/routes/create');
+
+const app = express();
+mongoose.Promise = global.Promise;
+mongoose.connect('localhost:27017/selftaught');
+
+// Parsers for POST data
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Point static path to dist
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+/**
+ * Get port from environment and store in Express.
+ */
+app.use(function(req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.setHeader('Access-Control-Allow-Methods', 'POST, GET, PATCH, DELETE, OPTIONS');
+    next();
+});
+
+// Set our api routes
+app.use('/', api);
+
+app.use(function(req, res, next) {
+    return res.render('index');
+});
+
+/**
+ * Get port from environment and store in Express.
+ */
+const port = process.env.PORT || '3000';
+app.set('port', port);
+
+/**
+ * Create HTTP server.
+ */
+const server = http.createServer(app);
+
+/**
+ * Listen on provided port, on all network interfaces.
+ */
+server.listen(port, () => console.log(`API running on localhost:${port}`));
+
+module.exports = app;
---
+++ src/app/app.routing.ts
@@ -0,0 +1,8 @@
+import { AppComponent } from './app.component';
+import { Routes, RouterModule } from '@angular/router';
+
+const APP_ROUTES: Routes = [
+
+];
+
+export const routing = RouterModule.forRoot(APP_ROUTES);
