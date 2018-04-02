c42c6fee4bd1a234e02d92e2213a7f98c92c0d46
jtaylor351 <jtaylor351@gatech.edu>
2018-04-02 17:29:32 -0400

    made login route

--- server/routes/user.js
+++ server/routes/user.js
@@ -33,26 +33,46 @@
 // });
 
 
-// router.post('/login', function(req, res, next) {
-//     User.findOne({ username: req.body.username }, function(err, user) {
-//         if (err) {
-//             return res.status(500).json({
-//                 title: 'An error occurred',
-//                 error: err
-//             });
-//         }
-//         if (!user) {
-//             return res.status(401).json({
-//                 title: 'Login failed',
-//                 error: { message: 'Invalid login credentials' }
-//             });
-//         }
-//         if (!bcrypt.compareSync(req.body.password, user.password)) {
-//             return res.status(401).json({
-//                 title: 'Login failed',
-//                 error: { message: 'Invalid login credentials' }
-//             });
-//         }
+router.post('/login', function(req, res, next) {
+    User.authenticate(req.body.email, req.body.email)
+    .then(function(user) {
+        var token = jwt.sign({ user: User }, 'secret', { expiresIn: 7200 });
+        return res.status(200).json({
+            message: 'Successfully logged in',
+            token: token,
+            email: user.email,
+            university: user.university,
+            firstName: user.firstName,
+            lastName: user.lastName,
+            bio: user.bio
+        });
+    })
+    .catch(function(err) {
+        return res.status(err.status).json({
+            title: 'Login failed',
+            error: { message: err.message }
+        });
+    });
+});
+
+        // if (err) {
+        //     return res.status(500).json({
+        //         title: 'An error occurred',
+        //         error: err
+        //     });
+        // }
+        // if (!user) {
+        //     return res.status(401).json({
+        //         title: 'Login failed',
+        //         error: { message: 'Invalid login credentials' }
+        //     });
+        // }
+        // if (!bcrypt.compareSync(req.body.password, user.password)) {
+        //     return res.status(401).json({
+        //         title: 'Login failed',
+        //         error: { message: 'Invalid login credentials' }
+        //     });
+        // }
 //         var token = jwt.sign({ user: User }, 'secret', { expiresIn: 7200 });
 //         res.status(200).json({
 //             message: 'Successfully logged in',
@@ -60,8 +80,8 @@
 //             userId: user._id,
 //             username: user.username
 //         });
-
+//
 //     });
 // });
 
-// module.exports = router;
+module.exports = router;
