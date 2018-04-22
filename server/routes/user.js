10b25a187a857d4078d9f96475847fa4a4c5909d
Garrett Smiley <garrettsmiley@msn.com>
2018-04-22 16:15:29 -0400

    merging garrett with master

--- server/routes/user.js
+++ server/routes/user.js
@@ -32,84 +32,84 @@ router.post('/signup', function(req, res, next) {
 router.post('/login', function(req, res, next) {
     console.log('LOGIN');
     User.authenticate(req.body.email)
-    .then(function(user) {
-        if (!user) {
-            return res.status(401).json({
-                title: 'Login failed',
-                error: { message: 'Invalid login credential'}
-            });
-        }
-        // console.log(user);
-        bcrypt.compare(req.body.password, user.password)
-        .then(function(res) {
-            // res == true
-            var token = jwt.sign({ user: User }, 'secret', { expiresIn: 7200 });
-            return res.status(200).json({
-                message: 'Successfully logged in',
-                token: token,
-                email: user.email,
-                university: user.university,
-                firstName: user.firstName,
-                lastName: user.lastName,
-                bio: user.bio
-            });
+        .then(function(user) {
+            if (!user) {
+                return res.status(401).json({
+                    title: 'Login failed',
+                    error: { message: 'Invalid login credential' }
+                });
+            }
+            // console.log(user);
+            bcrypt.compare(req.body.password, user.password)
+                .then(function(res) {
+                    // res == true
+                    var token = jwt.sign({ user: User }, 'secret', { expiresIn: 7200 });
+                    return res.status(200).json({
+                        message: 'Successfully logged in',
+                        token: token,
+                        email: user.email,
+                        university: user.university,
+                        firstName: user.firstName,
+                        lastName: user.lastName,
+                        bio: user.bio
+                    });
+                })
+                .catch(function(err) {
+                    return res.status(401).json({
+                        title: 'Login failed',
+                        error: { message: err.message }
+                    });
+                });
+            // bcrypt.compare(req.body.password, user.password, function(err, result) {
+            //     if (err) {
+            //         console.console.log("got where i should");
+            //         // var err = new Error('Invalid Password');
+            //         // err.status = 401;
+            //
+            //     }
+            //     if (result) {
+            //         var token = jwt.sign({ user: User }, 'secret', { expiresIn: 7200 });
+            //         return res.status(200).json({
+            //             message: 'Successfully logged in',
+            //             token: token,
+            //             email: user.email,
+            //             university: user.university,
+            //             firstName: user.firstName,
+            //             lastName: user.lastName,
+            //             bio: user.bio
+            //         });
+            //     }
+            // });
         })
-        .catch(function(err){
-            return res.status(401).json({
-                title: 'Login failed',
+        .catch(function(err) {
+            console.log("Server Side Problem");
+            console.log(err.message);
+            return res.status(err.status).json({
+                title: 'Problem on our end, please try again later',
                 error: { message: err.message }
             });
         });
-        // bcrypt.compare(req.body.password, user.password, function(err, result) {
-        //     if (err) {
-        //         console.console.log("got where i should");
-        //         // var err = new Error('Invalid Password');
-        //         // err.status = 401;
-        //
-        //     }
-        //     if (result) {
-        //         var token = jwt.sign({ user: User }, 'secret', { expiresIn: 7200 });
-        //         return res.status(200).json({
-        //             message: 'Successfully logged in',
-        //             token: token,
-        //             email: user.email,
-        //             university: user.university,
-        //             firstName: user.firstName,
-        //             lastName: user.lastName,
-        //             bio: user.bio
-        //         });
-        //     }
-        // });
-    })
-    .catch(function(err) {
-        console.log("Server Side Problem");
-        console.log(err.message);
-        return res.status(err.status).json({
-            title: 'Problem on our end, please try again later',
-            error: { message: err.message }
-        });
-    });
 });
-        // .then(function(user) {
-        //     var token = jwt.sign({ user: User }, 'secret', { expiresIn: 7200 });
-        //     return res.status(200).json({
-        //         message: 'Successfully logged in',
-        //         token: token,
-        //         email: user.email,
-        //         university: user.university,
-        //         firstName: user.firstName,
-        //         lastName: user.lastName,
-        //         bio: user.bio
-        //     });
-        // })
-        // .catch(function(err) {
-        //     console.log("\n\n\n printing error \n\n\n\n");
-        //     console.log(err);
-        //     res.status(401).json({
-        //         title: 'Login failed',
-        //         error: { message: err.message }
-        //     });
-        // });
+// .then(function(user) {
+//     var token = jwt.sign({ user: User }, 'secret', { expiresIn: 7200 });
+//     return res.status(200).json({
+//         message: 'Successfully logged in',
+//         token: token,
+//         email: user.email,
+//         university: user.university,
+//         firstName: user.firstName,
+//         lastName: user.lastName,
+//         bio: user.bio
+//     });
+// })
+// .catch(function(err) {
+//     console.log("\n\n\n printing error \n\n\n\n");
+//     console.log(err);
+//     res.status(401).json({
+//         title: 'Login failed',
+//         error: { message: err.message }
+//     });
+// });
 // });
 
 
